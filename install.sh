19ecb34a63319c8e41a7502c9411a626a875a0b2
Daniel Egeberg <daniel.egeberg@gmail.com>
2011-03-17 17:59:27 +0100

    now with symlinks

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# directories
+ln -s vim ~/.vim
+mkdir -p ~/.xmonad
+
+# files
+ln -s Xdefaults ~/.Xdefaults
+ln -s vimrc ~/.vimrc
+ln -s zshrc ~/.zshrc
+ln -s xmodmap ~/.xmodmap
+ln -s xbindkeysrc ~/.xbindkeysrc
+ln -s xsession ~/.xsession
+ln -s gitconfig ~/.gitconfig
+ln -s screenrc ~/.screenrc
+ln -s xmobarrc ~/.xmobarrc
+ln -s xmonad/xmonad.hs ~/.xmonad/xmonad.hs
