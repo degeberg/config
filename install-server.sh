667152c3c4b1c0d683b38a7f5ebe6f24b7d99dc7
Daniel Egeberg <daniel.egeberg@gmail.com>
2016-05-27 19:19:37 +0200

    Add server-install.sh script
    Base install for servers that do not have a graphical environment.
    install.sh will call this.

---
+++ install-server.sh
@@ -0,0 +1,29 @@
+#!/bin/zsh
+
+DIR=$(pwd)
+
+# files
+ln -s $DIR/gitconfig ~/.gitconfig
+ln -s $DIR/screenrc ~/.screenrc
+ln -s $DIR/ssh/config ~/.ssh/config
+ln -s $DIR/ghci ~/.ghci
+
+# vim
+ln -s $DIR/vim ~/.vim
+ln -s $DIR/vimrc ~/.vimrc
+ln -s $DIR/vim ~/.config/nvim
+if [ ! -d ~/.vim/bundle/vundle ]; then
+    git clone http://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+fi
+
+# other config repositories
+cd ~/projects
+
+git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+setopt EXTENDED_GLOB
+for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
+    ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
+done
+ln -sf $DIR/zshrc ~/.zshrc
+ln -sf $DIR/zpreztorc ~/.zpreztorc
+wget https://files.degeberg.com/prompt_superlinh_setup -O ~/.zprezto/modules/prompt/functions/prompt_superlinh_setup
--- install.sh
+++ install.sh
@@ -13,32 +13,11 @@ ln -s $DIR/Xdefaults ~/.Xdefaults
 ln -s $DIR/xmodmap ~/.xmodmap
 ln -s $DIR/xbindkeysrc ~/.xbindkeysrc
 ln -s $DIR/xinitrc ~/.xinitrc
-ln -s $DIR/gitconfig ~/.gitconfig
-ln -s $DIR/screenrc ~/.screenrc
 ln -s $DIR/xmobarrc ~/.xmobarrc
 ln -s $DIR/pentadactylrc ~/.pentadactylrc
 ln -s $DIR/xmonad/xmonad.hs ~/.xmonad/xmonad.hs
-ln -s $DIR/ssh/config ~/.ssh/config
 ln -s $DIR/dunstrc ~/.config/dunst/dunstrc
 ln -s $DIR/ncmpcpp/config ~/.ncmpcpp/config
-ln -s $DIR/ghci ~/.ghci
 ln -s $DIR/mpd.conf ~/.config/mpd/mpd.conf
 
-# vim
-ln -s $DIR/vim ~/.vim
-ln -s $DIR/vimrc ~/.vimrc
-ln -s $DIR/vim ~/.config/nvim
-if [ ! -d ~/.vim/bundle/vundle ]; then
-    git clone http://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
-fi
-
-# other config repositories
-cd ~/projects
-
-git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
-setopt EXTENDED_GLOB
-for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
-    ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
-done
-ln -sf $DIR/zshrc ~/.zshrc
-ln -sf $DIR/zpreztorc ~/.zpreztorc
+zsh ./install-server.sh
