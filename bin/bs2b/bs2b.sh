c9cfa83dc17d487feb77b0b3d092125228e844a3
Daniel Egeberg <daniel.egeberg@gmail.com>
2016-11-16 20:27:55 +0100

    make bs2b.sh more robust

--- bin/bs2b/bs2b.sh
+++ bin/bs2b/bs2b.sh
@@ -6,8 +6,12 @@ CMD=$2
 BS2B_SINK="crossfeed-bs2b-$SINK"
 
 usage() {
-    echo "usage: $0 sink [start|stop]"
+    (>&2 echo "usage: $0 sink [start|stop]")
     exit 1
+}
+
+sink_index() {
+    pacmd list-sinks | grep 'name:.*'"$1" -B1 | grep index | awk '{ print $2 }'
 }
 
 if [ "x$SINK" = "x" ]; then
@@ -16,11 +20,21 @@ fi
 
 case "$CMD" in
     start)
-        pacmd load-module module-ladspa-sink sink_name="$BS2B_SINK" master="$SINK" plugin=bs2b label=bs2b control=700,4.5
+        index=$(sink_index $SINK)
+        if [ "x$index" = "x" ]; then
+            (>&2 echo "Sink $SINK was not found. Cannot add bs2b.")
+            exit 1
+        else
+            pacmd load-module module-ladspa-sink sink_name="$BS2B_SINK" master="$SINK" plugin=bs2b label=bs2b control=700,4.5
+        fi
         ;;
     stop)
-        index=$(pacmd list-sinks | grep 'name:.*'"$BS2B_SINK" -B1 | grep index | awk '{ print $2 }')
-        pacmd kill-sink-input "$index"
+        index=$(sink_index $BS2B_SINK)
+        if [ "x$index" = "x" ]; then
+            (>&2 echo "Sink $BS2B_SINK was not found. Skipping removal.")
+        else
+            pacmd kill-sink-input "$index"
+        fi
         ;;
     *)
         usage
