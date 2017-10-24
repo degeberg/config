a31a8c67e2b627eb7097d633e25bc55f79461a51
Daniel Egeberg <daniel.egeberg@gmail.com>
2017-10-24 09:00:34 +0200

    stuff

--- bin/monitor/monitor-work-laptop.sh
+++ bin/monitor/monitor-work-laptop.sh
@@ -14,17 +14,20 @@ MODE=$1
 OUTPUT_MON1="DP2-3"
 OUTPUT_MON2="DP2-2"
 OUTPUT_PC="eDP1"
+CRTC="1"
 
 case "$MODE" in
     pc)
         xrandr --output $OUTPUT_PC --auto --primary \
             --output $OUTPUT_MON1 --off \
-            --output $OUTPUT_MON2 --off
+            --output $OUTPUT_MON2 --off \
+            --crtc $CRTC
         ;;
     dock)
         xrandr --output $OUTPUT_PC --auto --left-of $OUTPUT_MON1 \
             --output $OUTPUT_MON1 --auto --primary \
-            --output $OUTPUT_MON2 --auto --right-of $OUTPUT_MON1
+            --output $OUTPUT_MON2 --auto --right-of $OUTPUT_MON1 \
+            --crtc $CRTC
         ;;
     *)
         usage
